81e2f31c9d37ef7da01e670de1571451e5152646
Cody Fizette <cf2372@nyu.edu>
2019-05-26 22:01:12 -0400

    Implemented Permutation class

--- homework/02-deques-and-randomized-queues/Permutation.java
+++ homework/02-deques-and-randomized-queues/Permutation.java
@@ -4,10 +4,20 @@
  *  Description:
  **************************************************************************** */
 
-package PACKAGE_NAME;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
-
+        RandomizedQueue<String> randQueue = new RandomizedQueue<>();
+        int k = Integer.valueOf(args[0]);
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            randQueue.enqueue(item);
+        }
+        while (k > 0) {
+            StdOut.println(randQueue.dequeue());
+            k--;
+        }
     }
 }
